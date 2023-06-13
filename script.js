179b0e62463491b0cf2cdf715c48374ee814d756
SrLordGrim <127856317+SrLordGrim@users.noreply.github.com>
2023-06-13 17:42:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function searchNews(query) {
+    // Lógica para buscar noticias
+    // Retorna un arreglo de noticias basado en la consulta (query)
+    // Ejemplo de implementación:
+    const news = [
+      {
+        title: "Noticia 1",
+        description: "Descripción de la noticia 1",
+        link: "https://example.com/noticia1"
+      },
+      {
+        title: "Noticia 2",
+        description: "Descripción de la noticia 2",
+        link: "https://example.com/noticia2"
+      },
+      {
+        title: "Noticia 3",
+        description: "Descripción de la noticia 3",
+        link: "https://example.com/noticia3"
+      }
+      // Agrega más noticias en base a la búsqueda
+    ];
+  
+    return news;
+  }
+  
+  function performSearch(event) {
+    event.preventDefault();
+    const searchInput = document.querySelector('input[name="search"]');
+    const query = searchInput.value;
+    const searchResults = document.querySelector('.search-results');
+    searchResults.innerHTML = ''; // Limpiar los resultados anteriores
+  
+    const news = searchNews(query);
+  
+    if (news.length > 0) {
+      const resultsTitle = document.createElement('h2');
+      resultsTitle.textContent = 'Resultados de búsqueda';
+      searchResults.appendChild(resultsTitle);
+  
+      const resultsList = document.createElement('ul');
+  
+      news.forEach((article) => {
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = article.link;
+        link.textContent = article.title;
+        listItem.appendChild(link);
+        resultsList.appendChild(listItem);
+      });
+  
+      searchResults.appendChild(resultsList);
+    } else {
+      const noResults = document.createElement('p');
+      noResults.textContent = 'No se encontraron resultados.';
+      searchResults.appendChild(noResults);
+    }
+  }
+  
